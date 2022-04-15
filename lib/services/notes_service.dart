75dd36faf3791d2a9462d28c312f7b0623d134d8
myStery24 <kyleezihui@gmail.com>
2022-04-16 00:59:41 +0800

    Added comments

--- lib/services/notes_service.dart
+++ lib/services/notes_service.dart
@@ -1,15 +1,23 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:habit_note/model/notes_model.dart';
 import 'package:habit_note/utils/constants.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:path/path.dart';
 
 import '../main.dart';
 import 'base_service.dart';
 
 class NotesService extends BaseService {
+  FirebaseStorage _storage = FirebaseStorage.instance;
+  /// Reference to 'notes' collection
   NotesService({String? userID}) {
     ref = db.collection('notes');
   }
 
+  /// Notes
+  // Stream lets you to make changes in Firebase and update in app UI real-time
   Stream<List<NotesModel>> fetchNotes({String color = ''}) {
     // Get the snapshot of the document from Firebase and map to a list
     return color.isEmpty
@@ -25,4 +33,29 @@ class NotesService extends BaseService {
             },
           );
   }
+
+  /// Add images
+  Future<String> uploadImage(File? image) async {
+    String imageUrl = '';
+
+    if (image != null) {
+      String fileName = basename(image.path);
+      Reference storageRef = _storage.ref().child("notesImage/$fileName");
+
+      UploadTask uploadTask = storageRef.putFile(image);
+
+      await uploadTask.then((e) async {
+        await e.ref.getDownloadURL().then((value) async {
+          imageUrl = value;
+        });
+      });
+    }
+    log('network img');
+    log(imageUrl);
+
+    return imageUrl;
+  }
 }
+
+
+
