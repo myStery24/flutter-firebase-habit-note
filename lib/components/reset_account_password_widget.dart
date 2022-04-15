79a10a557262871f16bb7dae1640fdd75f5e0b23
myStery24 <kyleezihui@gmail.com>
2022-04-16 05:37:00 +0800

    Widget for showing info in change account password screen

---
+++ lib/components/reset_account_password_widget.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:habit_note/screens/password/forgot_password_screen.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+import '../utils/colours.dart';
+import '../utils/constants.dart';
+
+class ResetAccountPasswordWidget extends StatelessWidget {
+  const ResetAccountPasswordWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RichText(
+        textAlign: TextAlign.left,
+        text: TextSpan(
+          text: "Alternatively, you can reset through an ",
+          style: GoogleFonts.lato(
+            color: getBoolAsync(IS_DARK_MODE)
+                ? AppColors.kTextWhite
+                : AppColors.kTextBlack,
+            fontWeight: TextFontWeight.regular,
+          ),
+          children: [
+            TextSpan(
+              text: 'email link',
+              style: GoogleFonts.lato(
+                color: getBoolAsync(IS_DARK_MODE)
+                    ? AppColors.kHabitOrange
+                    : AppColors.kTextBlack,
+                fontSize: 16.0,
+                fontWeight: TextFontWeight.bold,
+              ),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  ForgotPasswordScreen().launch(context);
+                },
+            ),
+            TextSpan(text: '.'),
+          ],
+        ),
+      ),
+    );
+  }
+}
