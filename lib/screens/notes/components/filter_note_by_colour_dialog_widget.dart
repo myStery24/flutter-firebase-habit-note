9c1927b8fd6eee9bc00cf997af349d2be2b69ac2
myStery24 <kyleezihui@gmail.com>
2022-04-16 01:21:12 +0800

    Added shorten file path

--- lib/screens/notes/components/filter_note_by_colour_dialog_widget.dart
+++ lib/screens/notes/components/filter_note_by_colour_dialog_widget.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:habit_note/utils/common.dart';
-import 'package:habit_note/utils/string_constant.dart';
 import 'package:nb_utils/nb_utils.dart';
+
+import '../../../utils/constants.dart';
 
 class FilterNoteByColorDialogWidget extends StatefulWidget {
   final Function(String)? onColorTap;
