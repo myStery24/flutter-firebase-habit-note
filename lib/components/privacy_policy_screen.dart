a2c6ecfc92a9d703950ecab60bf7a75608d0bca6
myStery24 <kyleezihui@gmail.com>
2022-04-16 00:50:15 +0800

    Added privacy policy screen

---
+++ lib/components/privacy_policy_screen.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:habit_note/utils/colours.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class PrivacyPolicyScreen extends StatefulWidget {
+  static String tag = '/PrivacyPolicyScreen';
+
+  const PrivacyPolicyScreen({Key? key}) : super(key: key);
+
+  @override
+  State<PrivacyPolicyScreen> createState() => _PrivacyPolicyScreenState();
+}
+
+class _PrivacyPolicyScreenState extends State<PrivacyPolicyScreen> {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      backgroundColor: AppColors.kHabitBackgroundLightGrey,
+      appBar: AppBar(
+        title: Text('Privacy Policy'),
+      ),
+      body: SafeArea(
+        child: Container(
+          height: size.height,
+          width: size.width,
+          child: Column(
+            children: [
+              SizedBox(height: size.width * 0.008),
+              Container(
+                width: size.width,
+                margin: EdgeInsets.symmetric(horizontal: size.width * 0.035),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.policy_outlined,
+                      size: size.height * 0.075,
+                      color: AppColors.kHabitOrange,
+                    ),
+                    SizedBox(width: size.width * 0.04),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'HaBIT Note',
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.lato(
+                              fontSize: size.height * 0.025,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.kTextBlack.withOpacity(0.85),
+                            ),
+                          ),
+                          Text(
+                            'Last Updated: Apr 10, 2022',
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.lato(
+                              fontSize: size.height * 0.017,
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.kTextBlack.withOpacity(0.65),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: size.width * 0.015),
+              Expanded(
+                child: FutureBuilder(
+                    future: rootBundle.loadString('assets/privacy_policy.md'),
+                    builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                      if (snapshot.hasData) {
+                        return Markdown(
+                          selectable: false,
+                          data: snapshot.requireData,
+                          onTapLink: (text, href, title) {
+                            href != null ? launch(href) : null;
+                          },
+                        );
+                      }
+                      return Center(
+                        /// Markdown text displays here
+                        child: Container(),
+                      );
+                    }),
+              ),
+              SizedBox(height: size.height * 0.015),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
