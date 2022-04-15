4fe8fe1ffc965b0eaf19c984559446cb86a3c039
myStery24 <kyleezihui@gmail.com>
2022-04-16 01:29:51 +0800

    Renamed file paths

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,14 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
-import 'screens/SplashScreen.dart';
+import 'screens/splash_screen.dart';
 import 'services/auth_service.dart';
 import 'services/notes_service.dart';
 import 'services/notification_manager.dart';
 import 'services/subscription_service.dart';
 import 'services/user_db_service.dart';
-import 'store/AppStore.dart';
-import 'utils/app_theme.dart';
+import 'store/app_store.dart';
+import 'utils/theme.dart';
 import 'utils/constants.dart';
 import 'utils/string_constant.dart';
 
@@ -30,7 +30,6 @@ NotificationManager manager = NotificationManager();
 UserDBService userService = UserDBService();
 
 Future<void> main() async {
-  // TODO: Licensing Fonts: https://pub.dev/packages/google_fonts
   WidgetsFlutterBinding.ensureInitialized();
 
   defaultRadius = 8.0;
@@ -38,18 +37,21 @@ Future<void> main() async {
 
   await initialize();
 
+  /// Initialize Firebase
   await Firebase.initializeApp().then((value) {
     // .instance.initialize();
   });
   tz.initializeTimeZones();
 
+  /// Set state for app theme
   if (getBoolAsync(IS_DARK_MODE, defaultValue: false)) {
     appStore.setDarkMode(true);
   } else {
     appStore.setDarkMode(false);
   }
 
-  appStore.setLoggedIn(getBoolAsync(IS_LOGGED_IN));
+  /// Get the user login state
+  appStore.setLoggedIn(getBoolAsync(IS_LOGGED_IN)); // either true or false
 
   runApp(HaBITNoteApp());
 }
