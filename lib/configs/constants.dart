da3fb094b10e533d932b9e4d9c949db90b0e71d1
myStery24 <kyleezihui@gmail.com>
2022-05-30 20:21:56 +0800

    Update constants.dart

--- lib/configs/constants.dart
+++ lib/configs/constants.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'colors.dart';
 
+const kAnimationDuration = Duration(milliseconds: 200);
+const imageRadius = 40.0;
+
 /// Text
 class TextFontWeight {
   static const thin = FontWeight.w100;
@@ -16,8 +19,8 @@ class AppStrings {
   // App name
   static const String appName = 'HaBIT Note';
   // App version
-  static const String appVersionText = 'V0.0.4';
-  static const String appVersionText2 = 'Version 0.0.4';
+  static const String appVersionText = 'V0.0.5';
+  static const String appVersionText2 = 'Version 0.0.5';
   // App copyright
   static const String appCopyright = '© Copyright HABIT 2022. All rights reserved';
 
@@ -45,29 +48,6 @@ class AppStrings {
   static const String pressAgain = 'Press back again to exit app';
 }
 
-// Login Type
-const LoginTypeApp = 'app';
-const LoginTypeGoogle = 'google';
-const LoginTypeApple = 'apple';
-
-const GRID_VIEW = 'GRID_VIEW';
-const GRID_VIEW_2 = 'GRID_VIEW_2';
-const LIST_VIEW = 'LIST_VIEW';
-
-const IS_LOGGED_IN = 'IS_LOGGED_IN';
-const IS_FIRST_TIME = 'IS_FIRST_TIME';
-const NOTE_ID = 'NOTE_ID';
-const USER_ID = 'USER_ID';
-const LABEL_ID = 'LABEL_ID';
-const USERNAME = 'USERNAME';
-const PASSWORD = 'PASSWORD';
-const USER_EMAIL = 'USER_EMAIL';
-const USER_DISPLAY_NAME = 'USER_DISPLAY_NAME';
-const USER_PHOTO_URL = 'USER_PHOTO_URL';
-const USER_MASTER_PWD = 'USER_MASTER_PWD';
-const IS_DARK_MODE = 'IS_DARK_MODE';
-const LOGIN_TYPE = 'LOGIN_TYPE';
-
 class AppImages {
   // Images route (directory)
   static const String imageDir = 'assets/images';
@@ -87,9 +67,6 @@ class AppImages {
   static const onboard2 = '$imageDir/onboard2.png';
   static const success = '$imageDir/success.svg';
 }
-
-const kAnimationDuration = Duration(milliseconds: 200);
-const imageRadius = 40.0;
 
 class SizeConfig {
   static late MediaQueryData _mediaQueryData;
@@ -127,52 +104,88 @@ OutlineInputBorder outlineInputBorder() {
   );
 }
 
+// Login Type
+const LoginTypeApp = 'app';
+const LoginTypeGoogle = 'google';
+const LoginTypeApple = 'apple';
+const IS_LOGGED_IN = 'IS_LOGGED_IN';
+const IS_FIRST_TIME = 'IS_FIRST_TIME';
+const NOTE_ID = 'NOTE_ID';
+const USER_ID = 'USER_ID';
+const LABEL_ID = 'LABEL_ID';
+const USERNAME = 'USERNAME';
+const PASSWORD = 'PASSWORD';
+const USER_EMAIL = 'USER_EMAIL';
+const USER_DISPLAY_NAME = 'USER_DISPLAY_NAME';
+const USER_PHOTO_URL = 'USER_PHOTO_URL';
+const USER_MASTER_PWD = 'USER_MASTER_PWD';
+const IS_DARK_MODE = 'IS_DARK_MODE';
+const LOGIN_TYPE = 'LOGIN_TYPE';
+
+// Note views
+const GRID_VIEW = 'GRID_VIEW';
+const GRID_VIEW_2 = 'GRID_VIEW_2';
+const LIST_VIEW = 'LIST_VIEW';
 const FIT_COUNT = 'FIT_COUNT';
 const CROSS_COUNT = 'CROSS_COUNT';
 const SELECTED_LAYOUT_TYPE_DASHBOARD = 'SELECTED_LAYOUT_TYPE_DASHBOARD';
 
-// duration
+// Duration
 const DAY = 'Day';
 const WEEK = 'Week';
 const MONTH = 'Month';
 const YEAR = 'Year';
 
-const ringgit_icon = 'RM';
-const date_format = 'yyyy-MM-dd';
-const datetime_format = 'yyyy-MM-dd HH:mm a';
-
-const delete_account = 'Are you sure you want to delete your account? By deleting your account, it will become inaccessible, unrecoverable, and permanently closed.';
-const about = 'About';
+// Others
+const name = 'Name';
+const with_social_media = 'with social media';
+const continue_with_google = 'Continue with Google';
+const continue_with_apple = 'Continue with apple';
+const email_already_exits = 'This email is already exists';
+const forgot_pwd = 'Forgot Password';
 const dark_mode = 'Dark Mode';
-const help = 'Help';
-const ocr = 'OCR';
 const home_empty_note = 'Create your first note !';
 const add_note = 'Note';
 const add_todo = 'To-do';
+const delete_note = 'Delete note';
+const delete_todo = 'Delete to-do';
+const select_colour = 'Select colour';
+const delete = 'Delete';
+const password = 'Password';
+const cancel = 'Cancel';
+const submit = 'Submit';
+const reset_filter_text = 'Reset';
+const type_something_here = 'Type something awesome here';
+const type_something_here2 = "What's your to-do ?";
+const confirm_to_delete_note = 'Confirm to delete note ?';
+const select_option = 'Select option';
+const unlock_note = 'Unlock note';
+const lock_note = 'Lock note';
+const lock_unlock_note = 'Lock & Unlock Note';
+const create_lock_note_pwd = 'Create a master password';
+const select_layout = 'Select layout';
+const lock_notes_pwd = 'Password';
+const reset_acc_pwd = 'RESET ACCOUNT PASSWORD';
+const enter_master_pwd = 'Enter master password';
+const change_master_pwd = 'Change Master Password';
+const change_master_pwd2 = 'CHANGE MASTER PASSWORD';
+const labels = 'Labels';
+const edit = 'Edit';
+const help = 'Help';
+const ocr = 'OCR';
 const add_todo_image = 'Add Image';
 const from_camera = 'Take photo';
 const from_gallery = 'Choose photo';
 const clear = 'Cleared';
 const no_image = 'Error, please upload an image';
-const delete_note = 'Delete note';
-const delete_todo = 'Delete to-do';
-const select_colour = 'Select colour';
-const reminder_empty_note = 'Create your first reminder !';
-const sub_reminder = 'Subscription Reminder';
-const reset_acc_pwd = 'RESET ACCOUNT PASSWORD';
-const lock_notes_pwd = 'Password';
-const change_master_pwd = 'Change Master Password';
-const change_master_pwd2 = 'CHANGE MASTER PASSWORD';
-const enter_master_pwd = 'Enter master password';
 const settings_change_master_pwd = 'Change Master Password';
 const settings_change_pwd = 'Change Account Password';
+const about = 'About';
 const settings_info_about_us = 'About Us';
 const settings_info_pp = 'Privacy Policy';
 const settings_info_tou = 'Terms of Use';
 const support_email = 'Support Email';
-const log_out = 'Log Out';
-const log_out_text = 'Are you sure you want to log out ?';
-const invalid_pwd = 'Invalid/ wrong password';
+const invalid_pwd = 'Invalid/wrong password';
 const pwd_length = 'Password length must be 4';
 const pwd_not_same = 'Password not same';
 const confirm_pwd = 'Confirm password';
@@ -184,17 +197,17 @@ const change_acc_pwd = 'Change Account Password';
 const change_acc_pwd2 = 'CHANGE ACCOUNT PASSWORD';
 const pwd_changed_successfully = 'Password changed successfully';
 const pwd_reset_successfully = 'Password reset successfully';
+const log_out = 'Log Out';
+const log_out_text = 'Are you sure you want to log out ?';
+const reminder_empty_note = 'Create your first reminder !';
+const sub_reminder = 'Subscription Reminder';
 const add_subscription = 'Add Subscription';
-const delete = 'Delete';
 const update = 'Update';
 const save = 'SAVE';
-const password = 'Password';
-const cancel = 'Cancel';
-const submit = 'Submit';
-const edit = 'Edit';
-const reset_filter_text = 'Reset';
 const MYR = 'MYR';
-const name = 'Name';
+const ringgit_icon = 'RM';
+const date_format = 'yyyy-MM-dd';
+const datetime_format = 'yyyy-MM-dd HH:mm a';
 const expired = 'Expired';
 const description = 'Description';
 const recurring = 'Recurring';
@@ -203,30 +216,16 @@ const billing_period = 'Billing period';
 const first_payment = 'First payment';
 const exp_date = 'Expiry Date';
 const pay_method = 'Payment method';
-const share_note_change_not_allow = 'This is shared note, changes are not allow';
-const collaborator = 'Collaborator';
-const collaborators = 'Collaborators';
 const shared_by = 'Shared By';
-const type_something_here = 'Type something awesome here';
-const type_something_here2 = "What's your to-do ?";
-const confirm_to_delete_note = 'Confirm to delete note ?';
-const select_option = 'Select option';
-const unlock_note = 'Unlock note';
-const lock_note = 'Lock note';
-const lock_unlock_note = 'Lock & Unlock Note';
-const create_lock_note_pwd = 'Create a master password';
-const select_layout = 'Select layout';
-const forgot_pwd = 'Forgot Password';
-const email_already_exits = 'This email is already exists';
 const notification = 'Notification';
 const on_same_day = 'On the Same day';
-const with_social_media = 'with social media';
-const continue_with_google = 'Continue with Google';
-const continue_with_apple = 'Continue with apple';
 const next_payment = 'Next payment';
 const subscription_exp = 'Subscription expired. No notification available';
 const subscription_detail = 'Subscription Detail';
-const labels = 'Labels';
+const share_note_change_not_allow = 'This is shared note, changes are not allow';
+const collaborator = 'Collaborator';
+const collaborators = 'Collaborators';
+const delete_account = 'Are you sure you want to delete your account? By deleting your account, it will become inaccessible, unrecoverable, and permanently closed.';
 
 /// Testing devices
 List<String> testDevices = [''];
