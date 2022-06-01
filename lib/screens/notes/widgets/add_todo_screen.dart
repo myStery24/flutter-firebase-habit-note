9e7c90f06b43831b9d44697d9cf88731de72b87e
myStery24 <kyleezihui@gmail.com>
2022-06-01 13:24:36 +0800

    Update add_todo_screen.dart

--- lib/screens/notes/widgets/add_todo_screen.dart
+++ lib/screens/notes/widgets/add_todo_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:material_tag_editor/tag_editor.dart';
 import 'package:nb_utils/nb_utils.dart';
 
 import '../../../../main.dart';
 import '../../../configs/colors.dart';
 import '../../../configs/common.dart';
 import '../../../configs/constants.dart';
+import '../../../models/labels_model.dart';
 import '../../../models/notes_model.dart';
+import '../../../widgets/custom_chips.dart';
 import '../../dashboard/dashboard_screen.dart';
-import 'note_collaborator_screen.dart';
 
+/// TODO : Add labels to note, save to Firestore, map to the note
+/// ISSUE: The label does not save to database, currently display dummy data as labels
 class AddToDoScreen extends StatefulWidget {
   static String tag = '/AddToDoScreen';
 
@@ -23,13 +27,17 @@ class AddToDoScreen extends StatefulWidget {
 
 class AddToDoScreenState extends State<AddToDoScreen> {
   List<String> collaborateList = [];
+  List<CheckListModel> _checkList = [];
+  List<LabelsModel> _notesLabels = [];
+  List<String> _selectedLabels = ['To-dos'];
 
   TextEditingController todoController = TextEditingController();
   TextEditingController? textController;
+  TextEditingController notesLabelController = TextEditingController();
+
+  FocusNode _labelNode = FocusNode();
 
   Color? _kSelectColor;
-
-  List<CheckListModel> _checkList = [];
 
   bool _kIsUpdateTodo = false;
 
@@ -52,8 +60,8 @@ class AddToDoScreenState extends State<AddToDoScreen> {
     if (_kIsUpdateTodo) {
       _kSelectColor = getColorFromHex(widget.notesModel!.color!);
       _checkList.addAll(widget.notesModel!.checkListModel!);
+      _notesLabels.addAll(widget.notesModel!.noteLabel!);
     }
-
   }
 
   @override
@@ -63,7 +71,8 @@ class AddToDoScreenState extends State<AddToDoScreen> {
 
   @override
   void dispose() {
-    setStatusBarColor(appStore.isDarkMode ? AppColors.kPrimaryVariantColorDark : Colors.white,
+    setStatusBarColor(
+        appStore.isDarkMode ? AppColors.kPrimaryVariantColorDark : Colors.white,
         delayInMilliSeconds: 100);
     addToDoList();
     super.dispose();
@@ -89,6 +98,14 @@ class AddToDoScreenState extends State<AddToDoScreen> {
               finish(context);
             },
           ),
+          // IconButton(
+          //   icon: Icon(Icons.label_outline),
+          //   color: AppColors.kHabitDark,
+          //   onPressed: () async {
+          //     hideKeyboard(context);
+          //     LabelsListScreen().launch(context);
+          //   },
+          // ),
           IconButton(
             icon: Icon(Icons.more_vert_rounded),
             color: AppColors.kHabitDark,
@@ -107,19 +124,22 @@ class AddToDoScreenState extends State<AddToDoScreen> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              _kIsUpdateTodo && widget.notesModel!.collaborateWith!.first != getStringAsync(USER_EMAIL)
+              _kIsUpdateTodo &&
+                      widget.notesModel!.collaborateWith!.first !=
+                          getStringAsync(USER_EMAIL)
                   ? Row(
-                children: [
-                  Text('$shared_by :',
-                      style:
-                      boldTextStyle(color: Colors.black, size: 18)),
-                  4.width,
-                  Text(
-                      widget.notesModel!.collaborateWith!.first.validate(),
-                      style:
-                      boldTextStyle(color: Colors.black, size: 18)),
-                ],
-              ).paddingLeft(16)
+                      children: [
+                        Text('$shared_by :',
+                            style:
+                                boldTextStyle(color: Colors.black, size: 18)),
+                        4.width,
+                        Text(
+                            widget.notesModel!.collaborateWith!.first
+                                .validate(),
+                            style:
+                                boldTextStyle(color: Colors.black, size: 18)),
+                      ],
+                    ).paddingLeft(16)
                   : SizedBox(),
               addCheckListItemWidget(),
               Divider(indent: 16),
@@ -140,7 +160,7 @@ class AddToDoScreenState extends State<AddToDoScreen> {
                           onChanged: (val) {
                             setState(() {
                               checkListData.isCompleted =
-                              !checkListData.isCompleted!;
+                                  !checkListData.isCompleted!;
 
                               if (checkListData.isCompleted!) {
                                 _checkList.insert(
@@ -183,7 +203,75 @@ class AddToDoScreenState extends State<AddToDoScreen> {
                         )
                       ],
                     ).paddingTop(8);
-                  })
+                  }),
+            ],
+          ),
+        ),
+      ),
+
+      /// Note labels
+      bottomSheet: BottomAppBar(
+        color: Colors.transparent,
+        elevation: 0,
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(16, 6, 0, 6),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: TagEditor(
+                  maxLines: 1,
+                  controller: notesLabelController,
+                  // get the length of note label in database
+                  length: _selectedLabels.length,
+                  // length: snapshot.data!.length,
+                  focusNode: _labelNode,
+                  delimiters: [',', ' '],
+                  hasAddButton: false,
+                  resetTextOnSubmitted: true,
+                  // Text color of the label
+                  textStyle: TextStyle(
+                      // ? = is dark mode
+                      // : = is light mode
+                      color: getBoolAsync(IS_DARK_MODE)
+                          ? AppColors.kTextWhite
+                          : AppColors.kTextBlack),
+                  inputDecoration: const InputDecoration(
+                    hintText: 'Add labels...',
+                    hintStyle: TextStyle(color: AppColors.kHintTextLightGrey),
+                    hintMaxLines: 1,
+                    border: InputBorder.none,
+                  ),
+                  // save to database noteLabel field
+                  onSubmitted: (val) {
+                    setState(() {
+                      // _notesLabels.add(LabelsModel(labelName: val));
+                      _selectedLabels.add(val);
+                      toast(
+                          'Sorry, adding labels is not working in current version.');
+                      //snapshot.toString();
+                    });
+                  },
+                  onTagChanged: (newValue) {
+                    setState(() {
+                      _selectedLabels.add(newValue);
+                      //snapshot.toString();
+                    });
+                  },
+
+                  /// The label design
+                  tagBuilder: (context, index) => CustomChips(
+                    index: index,
+                    label: _selectedLabels[index],
+                    // label: snapshot.toString(),
+                    onDeleted: _onDelete,
+                  ),
+                  // InputFormatters example, this disallow \ and /
+                  inputFormatters: [
+                    FilteringTextInputFormatter.deny(RegExp(r'[/\\]'))
+                  ],
+                ),
+              ),
             ],
           ),
         ),
@@ -195,13 +283,16 @@ class AddToDoScreenState extends State<AddToDoScreen> {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        IconButton(icon: Icon(Icons.add, color: AppColors.kHintTextLightGrey), onPressed: null),
+        IconButton(
+            icon: Icon(Icons.add, color: AppColors.kHintTextLightGrey),
+            onPressed: null),
         TextField(
           autofocus: _kIsUpdateTodo ? false : true,
           controller: todoController,
           cursorColor: Colors.black,
           decoration: InputDecoration(
-              border: InputBorder.none, hintText: "What's on your mind ?",
+              border: InputBorder.none,
+              hintText: "What's on your mind ?",
               hintStyle: TextStyle(color: AppColors.kHintTextLightGrey)),
           style: primaryTextStyle(color: Colors.black),
           textCapitalization: TextCapitalization.sentences,
@@ -225,13 +316,14 @@ class AddToDoScreenState extends State<AddToDoScreen> {
   }
 
   void addToDoList() {
-    if (_checkList.isNotEmpty) {
+    if (_checkList.isNotEmpty || _notesLabels.isNotEmpty) {
       NotesModel notesData = NotesModel();
 
       notesData.userId = getStringAsync(USER_ID);
       notesData.checkListModel = _checkList;
       notesData.updatedAt = DateTime.now();
       notesData.collaborateWith = collaborateList.validate();
+      notesData.noteLabel = _notesLabels;
 
       if (_kSelectColor != null) {
         notesData.color = _kSelectColor!.toHex().toString();
@@ -242,8 +334,10 @@ class AddToDoScreenState extends State<AddToDoScreen> {
       if (_kIsUpdateTodo) {
         notesData.noteId = widget.notesModel!.noteId;
         notesData.createdAt = widget.notesModel!.createdAt;
-        notesData.collaborateWith = widget.notesModel!.collaborateWith.validate();
+        notesData.collaborateWith =
+            widget.notesModel!.collaborateWith.validate();
         notesData.isLock = widget.notesModel!.isLock;
+        notesData.noteLabel = [];
 
         notesService
             .updateDocument(notesData.toJson(), notesData.noteId)
@@ -260,6 +354,14 @@ class AddToDoScreenState extends State<AddToDoScreen> {
           toast(error.toString());
         });
       }
+      // if the list is empty
+    } else if (_checkList.isEmpty) {
+      // delete the to-do
+      notesService.removeDocument(widget.notesModel!.noteId).then((value) {
+        toast("To-do discarded");
+      }).catchError((error) {
+        toast(error.toString());
+      });
     }
   }
 
@@ -289,7 +391,7 @@ class AddToDoScreenState extends State<AddToDoScreen> {
                       notesService
                           .removeDocument(widget.notesModel!.noteId)
                           .then((value) {
-                        toast('To-do deleted');
+                        toast('To-do deleted forever');
                         finish(context);
                         DashboardScreen().launch(context, isNewTask: true);
                       }).catchError((error) {
@@ -301,6 +403,7 @@ class AddToDoScreenState extends State<AddToDoScreen> {
                   },
                 ),
               ),
+
               /// Collaborator
               // ListTile(
               //   leading: Icon(Icons.person_add_alt_1_rounded,
@@ -339,5 +442,13 @@ class AddToDoScreenState extends State<AddToDoScreen> {
         );
       },
     );
+  }
+
+  /// Delete the labels by pressing x
+  void _onDelete(index) {
+    setState(() {
+      toast('Label removed');
+      _selectedLabels.removeAt(index);
+    });
   }
 }
