7a06daf13da61139a51198373e2dee2de6296401
myStery24 <kyleezihui@gmail.com>
2022-06-01 13:21:59 +0800

    First commit

---
+++ lib/screens/help/widgets/custom_search_field.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../configs/colors.dart';
+import '../../../configs/constants.dart';
+
+class CustomSearchField extends StatefulWidget {
+  const CustomSearchField({
+    Key? key,
+    required this.hintField,
+    this.backgroundColor,
+  }) : super(key: key);
+
+  final String hintField;
+  final Color? backgroundColor;
+
+  @override
+  _CustomSearchFieldState createState() => _CustomSearchFieldState();
+}
+
+class _CustomSearchFieldState extends State<CustomSearchField> {
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Container(
+      width: size.width,
+      height: spacer,
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(5.0),
+      decoration: BoxDecoration(
+          color: widget.backgroundColor,
+          borderRadius: BorderRadius.circular(7.0)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          /// Search icon
+          Container(
+            height: 40.0,
+            width: 40.0,
+            alignment: Alignment.center,
+            child: Container(
+              child: SvgPicture.asset(
+                AppImages.search,
+                color: AppColors.kHintTextLightGrey.withOpacity(0.5),
+                height: 18.0,
+              ),
+            ),
+          ),
+
+          /// Search text field
+          Flexible(
+            child: Container(
+              width: size.width,
+              height: 38,
+              alignment: Alignment.topCenter,
+              child: TextField(
+                style: TextStyle(fontSize: 15),
+                cursorColor: AppColors.kTextBlack,
+                decoration: InputDecoration(
+                  hintText: widget.hintField,
+                  hintStyle: TextStyle(
+                    fontSize: 18.0,
+                    color: AppColors.kHintTextLightGrey.withOpacity(0.5),
+                  ),
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(width: 10.0),
+
+          /// Filter icon
+          Container(
+            height: 40.0,
+            width: 40.0,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: AppColors.kHabitOrange.withOpacity(0.7),
+              borderRadius: BorderRadius.circular(7.0),
+              boxShadow: [
+                BoxShadow(
+                  color: AppColors.kHabitOrange.withOpacity(0.5),
+                  spreadRadius: 0.0,
+                  blurRadius: 6.0,
+                  offset: Offset(0, 2),
+                )
+              ],
+            ),
+            child: Container(
+              child: SvgPicture.asset(
+                AppImages.filter,
+                color: AppColors.kTextWhite,
+                height: 13.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
