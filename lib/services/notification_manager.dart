4c50ad59ba9030b06c24f9e831337caa106ce30a
myStery24 <kyleezihui@gmail.com>
2022-06-06 12:50:42 +0800

    Bug fix: Add a notification logo to drawable

--- lib/services/notification_manager.dart
+++ lib/services/notification_manager.dart
@@ -5,6 +5,7 @@ import 'package:nb_utils/nb_utils.dart';
 import 'package:timezone/timezone.dart' as tz;
 
 class NotificationManager {
+  /// Initialize the plugin instance
   late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
   NotificationManager() {
@@ -12,51 +13,79 @@ class NotificationManager {
     initNotifications();
   }
 
+  /// Function for calling the notification
   void initNotifications() {
-    AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('app_logo');
-    IOSInitializationSettings initializationSettingsIOS = IOSInitializationSettings(onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    // Android
+    AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    // [projectFolder]/android/app/src/main/res/drawable
+    // iOS
+    IOSInitializationSettings initializationSettingsIOS =
+        IOSInitializationSettings(
+            onDidReceiveLocalNotification: onDidReceiveLocalNotification);
 
-    InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+    InitializationSettings initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
 
-    flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectNotification);
+    // Initialize Android and iOS settings
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
   }
 
-  Future<void> showScheduleNotification({required DateTime scheduledNotificationDateTime, String? title, String? description, required int id}) async {
+  Future<void> showScheduleNotification(
+      {required DateTime scheduledNotificationDateTime,
+      String? title,
+      String? description,
+      required int id}) async {
     log(scheduledNotificationDateTime);
 
     await cancelNotification(id);
 
-    var scheduleTime = tz.TZDateTime.from(scheduledNotificationDateTime, tz.local);
+    // the time to call out the notification
+    var scheduleTime =
+        tz.TZDateTime.from(scheduledNotificationDateTime, tz.local);
 
     log(scheduleTime);
 
     var androidDetails = AndroidNotificationDetails(
-      'channel_id',
-      'Channel Name',
-      // 'Channel Description',
-      // TODO: Find the logo in mighty notes
-      icon: 'app_logo2',
+      'id',
+      // change this id # if you change any settings else reinstall the app
+      'name',
+      // 'description',
+      priority: Priority.high,
+      importance: Importance.max, // show in banner
+      icon: '@mipmap/ic_launcher',
     );
     var iOSDetails = IOSNotificationDetails();
-    var platformDetails = NotificationDetails(android: androidDetails, iOS: iOSDetails);
+    var platformDetails =
+        NotificationDetails(android: androidDetails, iOS: iOSDetails);
 
+    // show the notification details
     await flutterLocalNotificationsPlugin.zonedSchedule(
       id,
       title,
-      '₹ $description due shortly',
+      'RM $description due shortly',
       scheduleTime,
       platformDetails,
       androidAllowWhileIdle: true,
-      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      payload: 'Default_Sound',
     );
   }
 
   Future onSelectNotification(String? payload) async {
-    print('Notification clicked');
+    if (payload != null) {
+      print('Notification payload: $payload');
+    } else {
+      print('Notification clicked');
+    }
     return Future.value(0);
   }
 
-  Future onDidReceiveLocalNotification(int id, String? title, String? body, String? payload) async {
+  Future onDidReceiveLocalNotification(
+      int id, String? title, String? body, String? payload) async {
+    // display a dialog with the notification details
     return Future.value(1);
   }
 
