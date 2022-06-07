7f5958186a414bcf47da9c4d1c49c8fa2897a2df
myStery24 <kyleezihui@gmail.com>
2022-06-07 13:56:42 +0800

    Move to widgets folder

--- lib/widgets/custom_search_field.dart
+++ lib/widgets/custom_search_field.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-import '../../../configs/colors.dart';
-import '../../../configs/constants.dart';
+import '../configs/colors.dart';
+import '../configs/constants.dart';
 
 class CustomSearchField extends StatefulWidget {
   const CustomSearchField({
