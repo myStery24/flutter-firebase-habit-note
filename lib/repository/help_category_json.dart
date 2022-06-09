e588e326723cf9e60841294c3fdec53b87607519
myStery24 <kyleezihui@gmail.com>
2022-06-09 17:13:48 +0800

    First commit

---
+++ lib/repository/help_category_json.dart
@@ -0,0 +1,20 @@
+import 'package:habit_note/configs/constants.dart';
+
+const List HelpCategoryJson = [
+  {
+    'title': 'Notes',
+    'icon': AppLottie.notesLottie,
+  },
+  {
+    'title': 'Image to text (OCR)',
+    'icon': AppLottie.ocrLottie,
+  },
+  {
+    'title': 'Subscription reminder',
+    'icon': AppLottie.reminderLottie,
+  },
+  {
+    'title': 'My account settings',
+    'icon': AppLottie.settingsLottie,
+  },
+];
