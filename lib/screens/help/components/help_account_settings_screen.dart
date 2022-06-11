205b144b4da961c59dbfd3e552003aa056faa7cf
myStery24 <kyleezihui@gmail.com>
2022-06-11 11:27:50 +0800

    Rename file

--- lib/screens/help/components/help_account_settings_screen.dart
+++ lib/screens/help/components/help_account_settings_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:habit_note/widgets/custom_button.dart';
+import 'package:habit_note/widgets/custom_buttons.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../../configs/colors.dart';
